50a8a26a90de38a83c175528d048395a8e5bcf05
Cristofy Meiru <meiru.cristofy05@gmail.com>
2026-06-17 12:20:43 -0300

    feat: return active hosts on network

--- scanner.py
+++ scanner.py
@@ -1,10 +1,15 @@
+import platform
 import subprocess
 from subprocess import CompletedProcess
 
+PING_PARAM: str = "-n" if platform.system().lower() == "windows" else "-c"
+
 
 class Scanner:
+
     def __init__(self, network_address: str) -> None:
         self.network_address = network_address
+        self.available_hosts = 0
 
     def get_network_prefix(self) -> str:
         result: list[str] = self.network_address.split(".")[:3]
@@ -17,22 +22,24 @@ class Scanner:
 
     def scan(self) -> None:
         address = self.get_network_prefix()
-        for i in range(0, 255):
+
+        print(f"Scanning network {address}.0/24...")
+
+        for i in range(1, 255):
             self.check_host(f"{address}.{i}")
 
+        print(f"{self.available_hosts} available hosts in local network.")
+        return
+
     def check_host(self, ip_address: str):
-        result: CompletedProcess[bytes] = subprocess.run(
-            ["ping", "-c", "1", ip_address]
+        result: CompletedProcess[str] = subprocess.run(
+            ["ping", PING_PARAM, "1", ip_address], capture_output=True, text=True
         )
-        print(result)
+        first = result.stdout.split("\n")[:2]
+        if result.returncode == 0:
+            self.available_hosts += 1
+            print(f"{first}\n")
 
-    def something(self):
-        result: CompletedProcess[bytes] = subprocess.run(
-            ["ping", "-c", "1", "192.168.1.1"], capture_output=True
-        )
-
-        if result.returncode == 0: 
-            print(result.stdout)
 
 if __name__ == "__main__":
     scanner = Scanner(network_address="192.168.1.0")
